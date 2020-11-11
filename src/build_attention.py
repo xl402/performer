eb6a89fbf477a5b53d86161978ab8748f5ac9219
Rory Xiao <rory@MacBook-Pro.local>
2020-11-11 18:06:26 +0000

    implemented build_linear_attention_equation and fiddled with test

--- src/build_attention.py
+++ src/build_attention.py
@@ -7,7 +7,16 @@ _CHR_IDX = string.ascii_lowercase
 
 
 def build_quadratic_attention_equation(rank, attn_axes):
+    # HERE:
+    # Source = Values (and Key dimensions)
+    # Product = intermediate
+    # Target = Query/Output
+    # Slight issue that the attention is not interpretable as A^{tilde} cannot be interpreted as attention
+
+    #import pdb; pdb.set_trace()
+    # what the final output should be
     target_notation = _CHR_IDX[:rank]
+    # tuple of batch dimensions (ALL) - attn_axes, penultimate (heads dim)
     batch_dims = tuple(np.delete(range(rank), attn_axes + (rank - 1,)))
     letter_offset = rank
     source_notation = ""
@@ -17,7 +26,7 @@ def build_quadratic_attention_equation(rank, attn_axes):
         else:
             source_notation += _CHR_IDX[letter_offset]
             letter_offset += 1
-
+    # 1. First group of axis are all batches (SAME), then multiply (target * source) attn_axes
     product_notation = "".join([target_notation[i] for i in batch_dims] +
                                [target_notation[i] for i in attn_axes] +
                                [source_notation[i] for i in attn_axes])
@@ -30,4 +39,35 @@ def build_quadratic_attention_equation(rank, attn_axes):
 
 
 def build_linear_attention_equation(rank, attn_axes):
-    raise(NotImplementedError)
+    # raise(NotImplementedError)
+    target_notation = _CHR_IDX[:rank]
+    # tuple of batch dimensions (ALL) - attn_axes, penultimate (heads dim)
+    batch_dims = tuple(np.delete(range(rank), attn_axes + (rank - 1,)))
+    letter_offset = rank
+
+    # construct V notation
+    v_notation = ""
+    for i in range(rank):
+        if i in batch_dims or i == rank - 1:
+            v_notation += target_notation[i]
+        else:
+            v_notation += _CHR_IDX[letter_offset]
+            letter_offset += 1
+
+    # construct K_tilde notation
+    k_notation = list(v_notation)
+    k_notation[-1] = 'r'
+    k_notation = ''.join(k_notation)
+
+    # construct K_tilde @ V notation
+    k_v_notation = "".join([target_notation[i] for i in batch_dims]) + 'r' + v_notation[-1]
+
+    # construct Q_tilde notation
+    q_notation = target_notation[:-1] + 'r'
+
+    k_v_product_equation = "%s,%s->%s" % (k_notation, v_notation,
+                                          k_v_notation)
+    attn_scores_rank = len(k_v_notation)
+    q_kv_product_equation = "%s,%s->%s" % (q_notation, k_v_notation,
+                                           target_notation)
+    return k_v_product_equation, q_kv_product_equation, attn_scores_rank
